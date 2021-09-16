3a037e7ddcb28e00c3b9a9142951ca17b86bd1bd
dariija <darija.bakanova@gmail.com>
2021-09-16 03:54:31 +0300

    feat: add solution for task Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,50 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+export default function transform(arr) {
+
+    if(!Array.isArray(arr)) {
+        throw new Error("'arr' parameter must be an instance of the Array!")
+    };
+
+    let basicArr = arr;
+    let newArr = [];
+
+    for (let i = 0; i < basicArr.length; i++) {
+      
+        switch(basicArr[i]) {
+            case '--discard-next':
+                if ( !(basicArr[i + 1] === undefined)) {
+                    basicArr.splice( i + 1, 1)
+                };
+                break;
+
+            case '--discard-prev': 
+                if ( !(basicArr[i - 1] === undefined)) {
+                  if( !(typeof basicArr[i - 1] === "string")) {
+                      newArr.pop()
+                  };
+                };
+                break;
+
+            case '--double-next':
+                if ( !(basicArr[i + 1] === undefined)) {
+                    newArr.push(basicArr[i + 1])
+                };
+                break;    
+
+            case '--double-prev': 
+                if ( !(basicArr[i - 1] === undefined)) {
+                    if( !(typeof basicArr[i - 1] === "string")) {
+                        newArr.push(basicArr[i - 1])
+                    };
+                };
+                break;
+
+            default:
+                newArr.push(basicArr[i]);
+        };
+    };
+    return newArr;
+};
