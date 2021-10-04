695a8f8d260baf052f0d18a55fd6ec1df23d2b4c
dariija <darija.bakanova@gmail.com>
2021-10-05 02:15:31 +0300

    feat: add solution for task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,76 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(value) {
+    if (!value) {
+        this.reverse = false;
+    } else {
+        this.reverse = true;
+    };
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+        throw new Error(`Incorrect arguments!`)
+    };
+
+    let alphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+    let res = [];
+
+    message = message.toUpperCase();
+    key = key.repeat( Math.ceil(message.length / key.length)).slice(0, message.length).toUpperCase();
+
+    for (let i = 0; i < message.length; i++ ) {
+        if (!/[A-Z]/.test(message[i])) {
+            key = key.slice(0, i) + `${message[i]}` + key.slice(i);
+        }
+    };
+    key = key.slice(0, message.length);
+
+    for(let i = 0; i < message.length; i++) {
+
+        if ( !/[A-Z]/.test(message[i])) {
+            res.push(message[i]);
+        } else {
+            let m = alphabet.indexOf(message[i]);
+            let k = alphabet.indexOf(key[i]);
+            let c = (m + k) % alphabet.length;
+            res.push(alphabet[c]);
+        }
+
+    };
+    return (this.reverse)? res.reverse().join("") : res.join("");
   }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+        throw new Error(`Incorrect arguments!`)
+    };
+
+    message = message.toUpperCase();
+    key = key.repeat( Math.ceil(message.length / key.length)).slice(0, message.length).toUpperCase();
+
+    for (let i = 0; i < message.length; i++ ) {
+        if (!/[A-Z]/.test(message[i])) {
+            key = key.slice(0, i) + `${message[i]}` + key.slice(i);
+        }
+    };
+
+    let alphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+    let res = [];
+
+    for(let i = 0; i < message.length; i++) {
+
+        if ( !/[A-Z]/.test(message[i])) {
+            res.push(message[i]);
+        } else {
+            let c = alphabet.indexOf(message[i]);
+            let k = alphabet.indexOf(key[i]);
+            let m = (c + alphabet.length - k) % alphabet.length;
+            res.push(alphabet[m]);
+        }
+    };
+    return (this.reverse)? res.reverse().join("") : res.join("");
+  };
 }
